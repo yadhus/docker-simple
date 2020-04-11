591cfcb7066bbbe31f36f768724ec99ef3bd5c48
Yadhunandan S <yadhus@users.noreply.github.com>
2020-04-11 23:13:54 +0530

    Travis CI first test push to heroku

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello world! How</p>
+        <p>Hello world! CI test heroku.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
