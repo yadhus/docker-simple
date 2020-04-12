bd096037a836948567bc2a2cfc1b7f1ad081456c
Yadhunandan S <yadhus@users.noreply.github.com>
2020-04-12 14:15:29 +0530

    Travis to heroku only production image push try

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello world! CI Travis to heroku works!</p>
+        <p>Hello world! Automatic deployment using travis to heroku</p>
         <a
           className="App-link"
           href="https://reactjs.org"
