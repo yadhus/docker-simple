13711789b7b001357ff32c18602ccf5fabfef2a5
Yadhunandan S <yadhus@users.noreply.github.com>
2020-04-12 13:37:13 +0530

    Travis to heroku only production image push try

--- release.sh
+++ release.sh
@@ -1,13 +1,9 @@
-curl --netrc -X PATCH https://api.heroku.com/apps/$APP_ID_OR_NAME/formation \
+curl --netrc -X PATCH https://api.heroku.com/apps/rocky-bayou-95129/formation \
   -d '{
   "updates": [
     {
       "type": "web",
       "docker_image": "$WEB_DOCKER_IMAGE_ID"
-    },
-    {
-      "type": "worker",
-      "docker_image": "$WORKER_DOCKER_IMAGE_ID"
     }
   ]
 }' \
