3df0f3dfdc162ce191a845d5603d6f0ebe5de9db
Yadhunandan S <yadhus@users.noreply.github.com>
2020-04-12 13:58:43 +0530

    Travis to heroku only production image push try

--- release.sh
+++ release.sh
@@ -1,11 +1,8 @@
-curl --netrc -X PATCH https://api.heroku.com/apps/rocky-bayou-95129/formation \
-  -d '{
-  "updates": [
-    {
-      "type": "web",
-      "docker_image": "$WEB_DOCKER_IMAGE_ID"
-    }
-  ]
-}' \
+imageId=$(docker inspect registry.heroku.com/rocky-bayou-95129/web --format={{.Id}})
+payload='{"updates":[{"type":"web","docker_image":"'"$imageId"'"}]}'
+
+curl -X PATCH https://api.heroku.com/apps/rocky-bayou-95129/formation \
+  -d "$payload" \
   -H "Content-Type: application/json" \
-  -H "Accept: application/vnd.heroku+json; version=3.docker-releases"
+  -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
+  -H "Authorization: Bearer $HEROKU_PASSWORD"
